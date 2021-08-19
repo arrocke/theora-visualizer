8d65a76fe272c6d0fced16390e0239d374b57b2f
Adrian Rocke <me@adrianrocke.com>
2021-08-19 17:15:26 -0500

    feat: decode ogg pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import getServerFile from "./load-file";
+import { decodePages } from "./ogg";
 
 function App() {
 
   React.useEffect(() => {
     getServerFile('/test.ogv').then(buffer => {
-      (window as any).view = new DataView(buffer)
+      const pages = decodePages(buffer)
+      console.log(pages)
+      ;(window as any).view = new DataView(buffer)
     })
   }, [])
 
---
+++ src/ogg.ts
@@ -0,0 +1,98 @@
+const MAGIC_NUMBER = 0x4f676753
+const VERSION = 0
+const CONTINUED_FLAG = 0x01
+const BOS_FLAG = 0x02
+const EOS_FLAG = 0x04
+const MAX_LACING_SIZE = 255
+const HEADER_SIZE = 27
+
+export interface OggPage {
+	flags: {
+		continued: boolean
+		bos: boolean
+		eos: boolean
+	}
+	granulePosition: DataView
+	serialNumber: number
+	pageNumber: number
+	checksum: number
+	packets: DataView[]
+	size: number
+}
+
+function validatePage(view: DataView, checksum: number): boolean {
+	return true
+}
+
+export function decodePage(buffer: ArrayBuffer, offset: number): OggPage {
+	const view = new DataView(buffer, offset)
+
+	const magicNumber = view.getUint32(0)
+	if (magicNumber !== MAGIC_NUMBER)
+		throw new Error('Page Decode Error')
+
+	const version = view.getUint8(4)
+	if (version !== VERSION)
+		throw new Error('Page Decode Error')
+
+	const headerType = view.getUint8(5)
+	const flags = {
+		continued: (headerType & CONTINUED_FLAG) === CONTINUED_FLAG,
+		bos: (headerType & BOS_FLAG) === BOS_FLAG,
+		eos: (headerType & EOS_FLAG) === EOS_FLAG
+	}
+
+	const granulePosition = new DataView(buffer, offset + 6, 8)
+	const serialNumber = view.getUint32(14)
+	const pageNumber = view.getUint32(18)
+	const checksum = view.getUint32(22)
+
+	const segmentCount = view.getUint8(26)
+	const packetSizes = []
+	let packetSize = 0
+	for (let i = 0; i < segmentCount; i++) {
+		const lacingSize = view.getUint8(27 + i)
+		packetSize += lacingSize
+		if (lacingSize < MAX_LACING_SIZE) {
+			packetSizes.push(packetSize)
+			packetSize = 0
+		}
+	}
+	if (packetSize > 0) {
+		packetSizes.push(packetSize)
+	}
+	const payloadSize = packetSizes.reduce((total, size) => total + size)
+	const pageSize = HEADER_SIZE + segmentCount + payloadSize
+
+	if (!validatePage(new DataView(buffer, offset, pageSize), checksum))
+		throw new Error('Page Decode Error')
+
+	const packets = []
+	let packetOffset = offset + HEADER_SIZE + segmentCount
+	for (const packetSize of packetSizes) {
+		packets.push(new DataView(buffer, packetOffset, packetSize))
+		packetOffset += packetSize
+	}
+
+	return {
+		flags,
+		granulePosition,
+		serialNumber,
+		pageNumber,
+		checksum,
+		packets,
+		size: pageSize
+	}
+}
+
+export function decodePages(buffer: ArrayBuffer): OggPage[] {
+	const pages: OggPage[] = []
+
+	for (let offset = 0; offset < buffer.byteLength;) {
+		const page = decodePage(buffer, offset)
+		pages.push(page)
+		offset += page.size
+	}
+
+	return pages
+}
